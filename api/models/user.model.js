459641d0bb8866972e78fa503a05fffdf43d39ce
yenru <yanru870119@gmail.com>
2024-01-14 20:21:29 +0800

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+}, {timestamps: true});
+
+const user = mongoose.model('User', userSchema);
+
+export default user;
